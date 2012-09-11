31fb927ac43bb46419f8261015850f20599ac545
robinharms <robin@betahaus.net>
2012-09-11 12:32:44 +0200

    Skeleton

--- voteit/whosonline/interfaces.py
+++ voteit/whosonline/interfaces.py
@@ -3,3 +3,24 @@ from zope.interface import Interface
 
 class IActivityUtil(Interface):
     """ A non-persistent storage of latest activities for users. """
+
+
+    def mark_activity_for(userid, meeting_uid, dt = None, **kw):
+        """ Mark activitiy for a userid within meeting_uid.
+            dt allows for the possibility to store another time than current UTC.
+            If any keywords are passed, the contents of them will be stored too.
+        """
+
+    def maybe_mark(self, request, context, dt = None, **kw):
+        """ Determine if context is within a meeting and wether there's a logged in user.
+            Allows for the same overrides as mark_activity_for.
+        """
+
+    def latest_activity(meeting_uid, limit = 5):
+        """ Return a tuple of dicts containing info on latest info within a meeting.
+            A limit value will cut off result. 0 means all.
+            
+            Returned dict will look something like:
+            
+            ({'fred':{'dt':<utc datetime>, 'someother_key': 'message'}}, <etc...>)
+        """
--- voteit/whosonline/models.py
+++ voteit/whosonline/models.py
@@ -1,5 +1,9 @@
 from BTrees.OOBTree import OOBTree
 from zope.interface import implements
+from voteit.core.models.date_time_util import utcnow
+from pyramid.security import authenticated_userid
+from pyramid.traversal import find_interface
+from voteit.core.models.interfaces import IMeeting
 
 from voteit.whosonline.interfaces import IActivityUtil
 
@@ -9,4 +13,25 @@ class ActivityUtil(object):
     implements(IActivityUtil)
     
     def __init__(self):
-        pass
+        self._storage = OOBTree()
+
+    def mark_activity_for(self, userid, meeting_uid, dt = None, **kw):
+        assert isinstance(meeting_uid, basestring)
+        kw['dt'] = dt and dt or utcnow()
+        try:
+            us = self._storage[meeting_uid][userid]
+        except KeyError:
+            if meeting_uid not in self._storage:
+                self._storage[meeting_uid] = OOBTree()
+            us = self._storage[meeting_uid][userid] = OOBTree()
+        for (k, v) in kw.items():
+            us[k] = v
+        return kw.keys()
+
+    def maybe_mark(self, request, context, dt = None, **kw):
+        userid = authenticated_userid(request)
+        if not userid:
+            return
+        meeting = find_interface(context, IMeeting)
+        if meeting:
+            self.mark_activity_for(userid, meeting.uid, dt = dt, **kw)
