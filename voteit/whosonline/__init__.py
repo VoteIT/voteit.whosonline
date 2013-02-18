56b6deb058749611e2a0ab9cb42a52702ec5d329
robinharms <robin@betahaus.net>
2013-02-18 22:01:32 +0100

    Adding icon + Adjusting to sidebar header

--- voteit/whosonline/__init__.py
+++ voteit/whosonline/__init__.py
@@ -9,6 +9,8 @@ def includeme(config):
     config.scan(PROJECTNAME)
     #Include translations
     config.add_translation_dirs('%s:locale/' % PROJECTNAME)
+    cache_ttl_seconds = int(config.registry.settings.get('cache_ttl_seconds', 7200))
+    config.add_static_view('whosonline_static', '%s:static' % PROJECTNAME, cache_max_age = cache_ttl_seconds)
     from .models import ActivityUtil
     util = ActivityUtil()
     config.registry.registerUtility(util)
